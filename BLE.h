ae035ef4a5fe8ffaff27d0e87f5871746a3a7446
Bao <baoq.tran@outlook.com>
2019-12-04 22:03:55 -0800

    Finished setting up UART1 to BLE communication. Command mode functional, need to do testing for transparent UART. Need to clean up main loop or internalize UART1 functions into BLE.h.

--- BLE.h
+++ BLE.h
@@ -7,9 +7,38 @@
 #include <stdint.h>
 #include <string.h>
 
-void BLE_init();
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+
+#include "driverlib/sysctl.h"
+
+#include "utils/UARTstdio.h"
+
+void BLE_init(void);
+void BLE_command(const char* pCommand);
 
 void BLE_init()
 {
+//	BLE_command("GK");
+//	SysCtlDelay(1000000);
+//	BLE_command("GNR");
+//	SysCtlDelay(1000000);
+//	BLE_command("C");
+//	SysCtlDelay(1000000);
+//	BLE_command("M");
+//	SysCtlDelay(1000000);
+}
+
+void BLE_command(const char* pCommand)
+{
 	
+	for (uint8_t i = 0; i < strlen(pCommand); i++)
+	{
+		UART1_DR_R = pCommand[i];
+	}
+	
+	UART1_DR_R = '\r';
+	
+	UARTprintf("> %s\n", pCommand);
 }
--- main.c
+++ main.c
@@ -27,6 +27,8 @@
 //}
 //#endif
 
+bool DEBOUNCE_FLAG = 0;
+
 void ConfigureUART(void)
 {
 	SYSCTL_RCGC1_R |= SYSCTL_RCGC1_UART0;								// Enable UART0 module.
@@ -46,20 +48,19 @@ void ConfigureUART(void)
 	
 	GPIO_PORTB_LOCK_R = 0x4C4F434B;											// Unlock PORTB.
 	GPIO_PORTB_CR_R = 0xFF;															// Allow changes to PORTB
+	
+	GPIO_PORTB_AMSEL_R = 0x00;
+	GPIO_PORTB_DEN_R |= GPIO_PIN_0 | GPIO_PIN_1;
 	GPIO_PORTB_AFSEL_R |= GPIO_PIN_0 | GPIO_PIN_1;			// Set PORTB0 and PORTB1 to alternate function.
 	GPIO_PORTB_DR2R_R |= GPIO_PIN_0 | GPIO_PIN_1;				// Set drive strengh to 2mA
+	GPIO_PORTB_PCTL_R |= 0x00000011;
 	
 	UART1_IBRD_R = 8;																		// Set baud rate to 115200 with 16MHz clock
 	UART1_FBRD_R = 54;
-	UART1_LCRH_R = 0x00000060;													// Set data length to 8 bits, enable FIFO.
-	UART1_CC_R = 0x00000005;														// Set clock source to PIOSC.
-	UART1_CTL_R |= UART_CTL_UARTEN;											// Enable UART1.
-	
-//	GPIOPinConfigure(GPIO_PB0_U1RX);
-//	GPIOPinConfigure(GPIO_PB1_U1TX);
-//	GPIOPinTypeUART(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-//	
-//	UARTClockSourceSet(UART1_BASE, UART_CLOCK_PIOSC);
+	UART1_LCRH_R = UART_LCRH_WLEN_8;										// Set data length to 8 bits.
+	UART1_LCRH_R |= UART_LCRH_FEN;											// Enable FIFO.
+	UART1_CC_R = UART_CC_CS_PIOSC;											// Set clock source to PIOSC.
+	UART1_CTL_R |= UART_CTL_RXE | UART_CTL_TXE | UART_CTL_UARTEN;
 }
 
 void ConfigureI2C(void)
@@ -74,10 +75,10 @@ void ConfigureI2C(void)
 	GPIO_PORTB_CR_R = 0xFF;															// Allow changes to PORTB
 	
 	GPIO_PORTB_AMSEL_R = 0x00;													// Disable analog
-	GPIO_PORTB_AFSEL_R = 0x0C;													// Enable alternate function for PORTB2 and PORTB3
-	GPIO_PORTB_ODR_R |= 0x08;														// Enable open drain for PORTB3 - I2C0SDA
-	GPIO_PORTB_DEN_R |= 0x0C;														// Enable digital I/O on PORTB2 and PORTB3
-	GPIO_PORTB_PCTL_R =0x00003300;											// Configure PMC for PORTB2 and PORTB3
+	GPIO_PORTB_AFSEL_R |= GPIO_PIN_2 | GPIO_PIN_3;			// Enable alternate function for PORTB2 and PORTB3
+	GPIO_PORTB_ODR_R |=  GPIO_PIN_3;										// Enable open drain for PORTB3 - I2C0SDA
+	GPIO_PORTB_DEN_R |= GPIO_PIN_2 | GPIO_PIN_3;				// Enable digital I/O on PORTB2 and PORTB3
+	GPIO_PORTB_PCTL_R |= 0x00003300;											// Configure PMC for PORTB2 and PORTB3
 	
 	I2C0_MCR_R = I2C_MCR_MFE;														// Initiailize I2C0 in master mode
 	I2C0_MTPR_R = 0x00000009;														// SCL clock speed set to 100Kbps
@@ -104,6 +105,34 @@ void ConfigurePORTF()
 	GPIO_PORTF_DATA_R &= ~0x0E;													// Turn off LEDs
 }
 
+void ConfigurePORTFInterrupts()
+{
+	GPIO_PORTF_IM_R &= ~0x11;														// Disable interrupts on PORTF0 and PORTF4
+	GPIO_PORTF_IS_R &= ~0x11;														// PORTF0 and PORTF4 configured edge sensitive
+	GPIO_PORTF_IBE_R &= ~0x11;													// PORTF0 and PORTF4 generation controlled by IEV register
+	GPIO_PORTF_IEV_R &= ~0x11;													// PORTF0 and PORTF4 configured falling edge
+	GPIO_PORTF_RIS_R = 0x00;														// Clear interrupt flag
+	NVIC_PRI7_R &= ~0x00E00000;
+	NVIC_EN0_R |= 0x40000000;														// Enable interrupt 30 in NVIC (GPIOF)
+	GPIO_PORTF_IM_R |= 0x11;														// Enable interrupts on PORTF0 and PORTF4
+}
+
+void Timer0_Init(unsigned long period)
+{
+	volatile unsigned long delay;
+	
+	SYSCTL_RCGC1_R |= SYSCTL_RCGC1_TIMER0;							// Activate TIMER0 module
+	delay = SYSCTL_RCGC1_R;															// Dummy read
+	TIMER0_CTL_R &= ~TIMER_CTL_TAEN;										// Disable TIMER0 during setup
+	TIMER0_CFG_R = 0x00000000;													// Configure for 32-bit mode
+	TIMER0_TAMR_R |= 0x00000001;												// Configure for one-shot mode
+	TIMER0_TAMR_R &= ~TIMER_TAMR_TACDIR;								// Configure for count down mode
+	TIMER0_TAILR_R = period - 1;												// Load start value
+	NVIC_PRI4_R &= ~0xE0000000; 	 											// configure Timer0 interrupt priority as 0
+	NVIC_EN0_R |= 0x00080000;     											// enable interrupt 19 in NVIC (Timer0A)
+	TIMER0_IMR_R |= TIMER_IMR_TATOIM;										// enable time out interrupt mask
+}
+
 int main()
 {
 	//
@@ -127,6 +156,10 @@ int main()
 	UARTprintf("I2C configured\n");
 	ConfigurePORTF();
 	UARTprintf("Tiva LEDs configured\n");
+	ConfigurePORTFInterrupts();
+	UARTprintf("Interrupts configured\n");
+	Timer0_Init(0xEFFFFF);
+	UARTprintf("Timers configured\n");
 	LCD_init();
 	UARTprintf("LCD initialized\n");
 	BLE_init();
@@ -138,26 +171,118 @@ int main()
 	// Turn off on board RGB LED.
 	//
 	GPIO_PORTF_DATA_R &= ~GPIO_PIN_1 & ~GPIO_PIN_2 & ~GPIO_PIN_3;
-	UART1_DR_R = 0xAA;
-	//UARTStdioConfig(1, 115200, 16000000);
-	//UARTprintf("Hello World!");
-//	
-//	I2C0_MDR_R = 0xAA;
+
+//	UARTprintf("Sending register address for read\n");
+//	I2C0_MDR_R = 0xC0;
 //	I2C0_MCS_R = I2C_MCS_STOP | I2C_MCS_START | I2C_MCS_RUN;
 //	
 //	while (I2C0_MCS_R & I2C_MCS_BUSBSY);														// Fix this maybe
 //	
-//	if (!(I2C0_MCS_R & I2C_MCS_ERROR))
+//	if (!(I2C0_MCS_R & I2C_MCS_ADRACK))
 //	{
-//		UARTprintf("Slave ACK received\n");
+//		UARTprintf("Slave address ACK received\n");
 //	}
 //	else
 //	{
-//		UARTprintf("Error receiving slave ACK\n");
+//		UARTprintf("Error receiving slave address ACK\n");
+//	}
+//	
+//	UARTprintf("Sending read request\n");
+//	I2C0_MSA_R = VL53L0X_ADDRESS + 1;
+//	I2C0_MCS_R = I2C_MCS_STOP | I2C_MCS_START | I2C_MCS_RUN;
+//	
+//	while (I2C0_MCS_R & I2C_MCS_BUSBSY);														// Fix this maybe
+//	
+//	if (!(I2C0_MCS_R & I2C_MCS_ADRACK))
+//	{
+//		UARTprintf("Slave address ACK received\n");
+//	}
+//	else
+//	{
+//		UARTprintf("Error receiving slave address ACK\n");
 //	}
 	
+//	for (int i = 0; i < 0xFE; i++)
+//	{
+	//	UARTprintf("Sending register address for read\n");
+	//	I2C0_MDR_R = 0xC0;
+	//	I2C0_MCS_R = I2C_MCS_STOP | I2C_MCS_START | I2C_MCS_RUN;
+	//	
+	//	while (I2C0_MCS_R & I2C_MCS_BUSBSY);														// Fix this maybe
+	//	
+	//	if (!(I2C0_MCS_R & I2C_MCS_ADRACK))
+	//	{
+	//		UARTprintf("Slave address ACK received\n");
+	//	}
+	//	else
+	//	{
+	//		UARTprintf("Error receiving slave address ACK\n");
+	//	}
+	//	
+	//	UARTprintf("Sending read request\n");
+	//	I2C0_MSA_R = VL53L0X_ADDRESS + 1;
+	//	I2C0_MCS_R = I2C_MCS_STOP | I2C_MCS_START | I2C_MCS_RUN;
+	//	
+	//	while (I2C0_MCS_R & I2C_MCS_BUSBSY);														// Fix this maybe
+	//	
+	//	if (!(I2C0_MCS_R & I2C_MCS_ADRACK))
+	//	{
+	//		UARTprintf("Slave address ACK received\n");
+	//		break;
+	//	}
+	//	else
+	//	{
+	//		UARTprintf("Error receiving slave address ACK\n");
+	//	}
+//	}
+	
+	char buffer[128] = "";
+	char receiveChar;
+	uint8_t i = 0;
+	
 	while (1)
-	{
+	{	
+		while (UART1_FR_R & UART_FR_RXFE);
 		
+		receiveChar = UART1_DR_R;
+		
+		if ((receiveChar == '%') || (receiveChar == '\r'))
+		{
+			UARTprintf("< %s\n", buffer);
+			strcpy(buffer, "");
+		}
+		else
+		{
+			strcat(buffer, &receiveChar);
+			i++;
+		}
 	}
+}
+
+void GPIOF_Handler(void)
+{
+	GPIO_PORTF_ICR_R |= 0x11;
+	
+	if (!(GPIO_PORTF_DATA_R & GPIO_PIN_4) && (DEBOUNCE_FLAG == 0))
+	{
+		BLE_command("$$$");
+		DEBOUNCE_FLAG = 1;
+		GPIO_PORTF_IM_R &= ~0x11;
+		TIMER0_CTL_R |= TIMER_CTL_TAEN;
+	}
+	else if (!(GPIO_PORTF_DATA_R & GPIO_PIN_0) && (DEBOUNCE_FLAG == 0))
+	{
+		BLE_command("");
+		DEBOUNCE_FLAG = 1;
+		GPIO_PORTF_IM_R &= ~0x11;
+		TIMER0_CTL_R |= TIMER_CTL_TAEN;
+	}
+}
+
+void TIMER0A_Handler(void)
+{
+	TIMER0_ICR_R |= TIMER_ICR_TATOCINT;									// Set acknowlegement flag for TIMER0
+	
+	DEBOUNCE_FLAG = 0;																	// Clear debounce
+	GPIO_PORTF_IM_R |= 0x11;
 }
